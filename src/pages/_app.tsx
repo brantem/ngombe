d5323dac22c0ca31bd808b582e3f910bb8d1a122
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2023-05-21 20:57:36 +0700

    Add next-pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,20 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Ngombe" />
+        <meta name="keywords" content="Ngombe" />
+        <title>Ngombe</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link href="/icons/icon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/icons/icon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta
           name="theme-color"
           content={
