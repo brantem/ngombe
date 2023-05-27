248a040813e1ffa31834a9b606fb1ff7584b8b21
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2023-05-27 22:58:32 +0700

    Fix NaN

--- src/lib/__tests__/helpers.test.ts
+++ src/lib/__tests__/helpers.test.ts
@@ -1,6 +1,8 @@
 import { calcPercentage } from 'lib/helpers';
 
 test('calcPercentage', () => {
+  expect(calcPercentage(0, 10)).toEqual(0);
+  expect(calcPercentage(10, 0)).toEqual(0);
   expect(calcPercentage(10, 20)).toEqual(50);
   expect(calcPercentage(10, 30)).toEqual(33);
 });
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -7,5 +7,6 @@ export const cn = (...inputs: ClassValue[]) => {
 };
 
 export const calcPercentage = (a: number, b: number) => {
+  if (a === 0 || b === 0) return 0;
   return Math.round((a / b) * 100);
 };
